a04416fda1859c24f722220571ff7aea49b6767f
vithya27 <vithya.shankar93@hotmail.com>
2022-11-07 22:54:36 +0800

    App Skeleton created

---
+++ yakker/components/Feed.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+
+const Feed = () => {
+  return (
+    <div className="col-span-7 lg:col-span-5 border-x">
+      <div className="flex items-center justify-between">
+        <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
+        <ArrowPathIcon className="h-6 w-6 mr-5 mt-5 cursor-pointer transition-all duration-500 ease-out hover:rotate-180  hover:text-yakker active:scale-125" />
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
---
+++ yakker/pages/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
+
+const Home: NextPage = () => {
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <Head>
+        <title>Yakker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="grid grid-cols-9">
+        <Sidebar />
+
+        <Feed />
+
+        <Widgets />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
